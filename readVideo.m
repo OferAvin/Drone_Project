a4ea4346761abe9de26ae58b9445817e187fe18c
Ofer Avin <oferavin32@gmail.com>
2020-09-16 16:11:45 +0300

    conflict fixed

--- readVideo.m
+++ readVideo.m
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-clear all;close all;
-vidObj = VideoReader('C:\Users\ofera\Studies\drone_proj\vid.mp4');
-
-th = 180;
-i = 1;
-
-%display frame by frame (play video)
-currAxes = axes;
-while hasFrame(vidObj)
-    vidFrame = readFrame(vidObj); %read next frame
-    r = vidFrame(:,:,1);
-    maxVal(i) = max(max(r(355:365,635:645)));
-    %   image(vidFrame, 'Parent',currAxes);
-    imshow(vidFrame, 'Parent',currAxes);
-    currAxes.Visible = 'off';
-    pause(1/vidObj.FrameRate);
-    i = i + 1;
-end
-bwVec = maxVal > th;
-changesVec = diff(bwVec);
-framePerChange = diff(find(changesVec ~= 0);
-
-allFrames = re(vidObj); %read all frames
-size(allFrames)
-
-r = vidFrame(:,:,1);
-g = vidFrame(:,:,2);
-b = vidFrame(:,:,3);
-I = rgb2gray(vidFrame);
-=======
 clear ; close all; clc
 
 VideoObject = VideoReader("C:\Users\ophir\Desktop\Uni\BCI\Drone_Project\ScreenFlickerCropped.mp4");
@@ -69,4 +38,3 @@ difference = diff(pks);
 figure(2)
 plot(f, signal_fft)
 
->>>>>>> b2d4edeba2418a5fc47f3d95cfdd485ef5b8f7ad
