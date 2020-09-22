195c12d422924af34b3a0915efee6ddde378409c
Ofer Avin <oferavin32@gmail.com>
2020-09-22 13:18:46 +0300

    readVideo changes

--- readVideo.m
+++ readVideo.m
@@ -1,12 +1,17 @@
 clear ; close all; clc
 curPath = pwd;
-vidPath = [curPath '\ScreenFlickerCropped.mp4'];
+vidPath = [curPath '\vid.mp4'];
 VideoObject = VideoReader(vidPath);
 i = 0; %Iteration intialization
 
 % video is 34.15 seconds long, divide by 8 is 4.268 seconds in real time.
 % 1024 frames divide by 4.268 is 239.92. Approx into 240 FPS.
-fps = 240; 
+fps = 30; 
+
+freqR = 6; %in HZ
+freqL = 5;
+freqU = 10;
+freqD = 3;
 
 %Allocations
 width = VideoObject.Width;
@@ -18,7 +23,32 @@ grayFrames = zeros(height, width, frames_N);
 while hasFrame(VideoObject)
     i = i + 1; %Iteration count
     currentFrame = readFrame(VideoObject);
+    if mod(i, fps/freqL) == 0
+        currentFrame(360:379,10:29,1) = 200;
+        currentFrame(360:379,10:29,2) = 40;
+        currentFrame(360:379,10:29,3) = 140;
+    end
+    
+    if mod(i, fps/freqR) == 0
+    currentFrame(360:379,1250:1269,1) = 200;
+    currentFrame(360:379,1250:1269,2) = 40;
+    currentFrame(360:379,1250:1269,3) = 140;
+    end
+
+    if mod(i, fps/freqU) == 0
+    currentFrame(10:29,640:659,1) = 200;
+    currentFrame(10:29,640:659,2) = 40;
+    currentFrame(10:29,640:659,3) = 140;
+    end
+
+    if mod(i, fps/freqD) == 0
+    currentFrame(690:709,640:659,1) = 200;
+    currentFrame(690:709,640:659,2) = 40;
+    currentFrame(690:709,640:659,3) = 140;
+    end
+
     grayFrames(:,:,i) = rgb2gray(currentFrame); 
+    imshow(currentFrame)
 end
 
 %Total values of the matrix (the higher it is, the whiter it is)
